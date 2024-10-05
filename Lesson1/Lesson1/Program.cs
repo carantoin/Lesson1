815853d66e2426c8f82318c34465df0032079773
carantoin <90127329+carantoin@users.noreply.github.com>
2024-10-05 18:05:34 +0200

    Add files via upload

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,13 @@
+﻿ try
+ {
+     Console.WriteLine("Введите x:");
+     double x = double.Parse(Console.ReadLine());
+     Console.WriteLine("Введите y:");
+     double y = double.Parse(Console.ReadLine());
+     double F = Math.Sqrt((2 + y) * (2 + y) + Math.Pow(7, Math.Sqrt(Math.Sin(y + 5)))) / Math.Log((x + 1)) - Math.Pow(3,y);
+     Console.WriteLine($"F={F:F2}");
+ }
+ catch
+ {
+     Console.WriteLine("Введите правильное значение!");
+ }     
